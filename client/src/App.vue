28fdd8cf513e83524baf0d15149c247be66cae03
wutianyang <wutianyang@hd123.com>
2018-02-26 10:40:38 +0800

    添加前端地址

---
+++ client/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    created() {
+      this._initHttp()
+    },
+    methods: {
+      _initHttp() {
+        this.axios.defaults.baseURL = 'http://172.17.11.105:3001/'
+        this.axios.defaults.headers.common['Content-Type'] = 'application/json;charset=UTF-8'
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import "common/scss/reset";
+  @import "common/scss/element";
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
+
+  body {
+    background-color: #f5f7f9;
+  }
+</style>
---
+++ client/src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import router from './router'
+import axios from 'axios'
+
+Vue.config.productionTip = false
+Vue.prototype.axios = axios
+
+Vue.use(ElementUI)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: {App},
+  template: '<App/>'
+})

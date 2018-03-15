4ce1bcfd155a8892e4ca41c481b2a3a13c0dc877
zakiso <dzq1993@qq.com>
2018-03-15 15:19:35 +0800

    update team schema

---
+++ server/model/team.js
@@ -0,0 +1,44 @@
+const mongoose = require('../helper/db')
+const Schema = mongoose.Schema
+
+var teamSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        index: true
+    },
+    icon: String,
+    creatorId: {
+        type: String,
+        required: true
+    },
+    createAt: {
+        type: Date,
+        default: Date.now
+    },
+    apps: [
+        {
+            _id: ObjectId,
+            appName: String,
+            icon: String
+        }
+    ],
+    members: [
+        {
+            _id: ObjectId,
+            username: String,
+            role: {
+                type: String,
+                enum: ["owner", "manager", "guest"]
+            }
+        }
+    ]
+})
+
+
+function autoAddCreator(params) {
+    
+}
+
+module.exports = mongoose.model('Team', teamSchema);

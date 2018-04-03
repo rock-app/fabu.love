92fcf9c6dfa537ddbbc7ee064c5e6138950338e6
Darren-chenchen <1597887620@qq.com>
2018-04-03 20:57:09 +0800

    消息接口调用

--- server/controllers/message.js
+++ server/controllers/message.js
@@ -9,31 +9,6 @@ import { responseWrapper } from "../helper/util";
 import bcrypt from "bcrypt"
 import Fawn from "fawn"
 
-<<<<<<< HEAD
-// module.exports = class AppRouter {
-//   @request('get','/api/messages/{userId}')
-//   @summary("获取该用户未读消息列表")
-//   @query(
-//       {
-//       page:{type:'number',default:0,description:'分页页码(可选)'},
-//       size:{type:'number',default:10,description:'每页条数(可选)'}
-//   })
-//   @path({userId:{type:'string',description:'用户id,不传则获取该用户下的App'}})
-//   @tag
-//   static async getApps(ctx,next){
-//       var page = ctx.query.page || 0
-//       var size = ctx.query.size || 10
-//       var user = ctx.state.user.data;
-//       var { teamId } = ctx.validatedParams;        
-
-//       var result = await App.find(
-//               {'ownerId':teamId}
-//           ).limit(size).skip(page * size)
-//       ctx.body = responseWrapper(result)
-//   }
-// }
-=======
-
 const tag = tags(['消息']);
 
 
@@ -78,4 +53,3 @@ module.exports = class MessageRouter {
   }
 
 }
->>>>>>> 65bb672a0b03b3554b558bc393e73344d210fae6

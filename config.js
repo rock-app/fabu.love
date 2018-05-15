6912ca841a310a40bf8b5f636eee357c324e8660
zakiso <dzq1993@qq.com>
2018-05-15 17:23:24 +0800

    修改配置

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const common = {
-  baseUrl: 'http://119.29.62.204:9090',
-  port: 9090,
+  baseUrl: 'http://localhost:3009/',
+  port: 3009,
   apiPrefix:'api',
   fileDir:__dirname + '/uploaded' //上传文件的存放目录
 };

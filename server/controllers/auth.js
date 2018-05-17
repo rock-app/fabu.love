a9621a76d11d99e6eb1f29cb32c216562e1877e0
dengzhiqiang <dengzhiqiang@hd123.com>
2018-05-17 17:41:05 +0800

    修复问题

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,12 +1,13 @@
 'use strict';
 
-import {request, summary, tags, body,description} from '../swagger';
+import {request, summary, tags, body, description} from '../swagger';
 import {User, userSchema} from "../model/user";
 import Team from "../model/team"
-import { responseWrapper } from "../helper/util";
+import {responseWrapper} from "../helper/util";
 import bcrypt from "bcrypt"
 import Fawn from "fawn"
 import Mail from '../helper/mail'
+import config from '../config'
 
 const jwt = require('jsonwebtoken');
 
@@ -32,7 +33,7 @@ var registerSchema = {
         type: 'string',
         required: true
     },
-    email : {
+    email: {
         type: 'string',
         required: true
     }
@@ -56,12 +57,15 @@ module.exports = class AuthRouter {
             throw new Error('用户名或密码错误')
         }
         user.token = jwt.sign({
-            data: {_id:user._id,username:user.username,email:user.email},
+            data: {
+                _id: user._id,
+                username: user.username,
+                email: user.email
+            },
             exp: Math.floor(Date.now() / 1000) + (60 * 60)
-        }, 'jwt-secret')
+        }, config.secret)
         ctx.body = responseWrapper(user)
-    } 
-       
+    }
 
     @request('post', '/api/user/register')
     @summary('注册用户')
@@ -82,15 +86,17 @@ module.exports = class AuthRouter {
                 {
                     _id: newUser._id,
                     username: newUser.username,
-                    email:newUser.email,
+                    email: newUser.email,
                     role: "owner"
                 }
             ]
-            newUser.teams = [{
-                _id:team._id,
-                name:team.name,
-                role:"owner"
-            }]
+            newUser.teams = [
+                {
+                    _id: team._id,
+                    name: team.name,
+                    role: "owner"
+                }
+            ]
             var task = Fawn.Task();
             var result = await task
                 .save(team)
@@ -106,43 +112,59 @@ module.exports = class AuthRouter {
     @request('post', '/api/user/password/modify')
     @summary('修改用户密码')
     @body({
-        oldpwd:{type:'string',require:true},
-        newpwd:{type:'string',require:true}
+        oldpwd: {
+            type: 'string',
+            require: true
+        },
+        newpwd: {
+            type: 'string',
+            require: true
+        }
     })
     @tag
     static async modifyPassword(ctx, next) {
         var user = ctx.state.user.data;
         var body = ctx.request.body;
-        var userData = await User.findById(user._id,"password")
+        var userData = await User.findById(user._id, "password")
         if (!userData) {
             throw new Error("用户不存在");
-        } 
+        }
         let valide = await bcrypt.compare(body.oldpwd, userData.password);
         if (!valide) {
             throw new Error("密码错误");
         }
         body.password = await bcrypt.hash(body.newpwd, 10); // 10是 hash加密的级别, 默认是10，数字越大加密级别越高
-        await User.findByIdAndUpdate(user._id,{password:body.password})
+        await User.findByIdAndUpdate(user._id, {password: body.password})
         ctx.body = responseWrapper(true, "密码修改成功")
     }
 
     @request('post', '/api/user/modify')
     @summary('修改用户资料')
     @body({
-        mobile:{type:'string'},
-        qq:{type:'string'},
-        company:{type:'string'},
-        career:{type:'string'}
+        mobile: {
+            type: 'string'
+        },
+        qq: {
+            type: 'string'
+        },
+        company: {
+            type: 'string'
+        },
+        career: {
+            type: 'string'
+        }
     })
     @tag
     static async changeUserInfo(ctx, next) {
         var user = ctx.state.user.data
         var body = ctx.request.body
-        var userData = await User.findById(user._id,"username");
+        var userData = await User.findById(user._id, "username");
         if (!userData) {
             throw new Error("用户不存在");
-        } 
-        await User.updateOne({username:user.username}, {
+        }
+        await User.updateOne({
+            username: user.username
+        }, {
             mobile: body.mobile,
             qq: body.qq,
             company: body.company,
@@ -156,10 +178,10 @@ module.exports = class AuthRouter {
     @tag
     static async getUserInfo(ctx, next) {
         var user = ctx.state.user.data
-        var user = await User.findById(user._id,"-teams -password");
+        var user = await User.findById(user._id, "-teams -password");
         if (!user) {
             throw new Error("用户不存在");
-        } 
+        }
         ctx.body = responseWrapper(user)
     }
 
@@ -168,29 +190,42 @@ module.exports = class AuthRouter {
     @tag
     static async getuserTeams(ctx, next) {
         var user = ctx.state.user.data
-        var user = await User.findById(user._id,"teams");
+        var user = await User.findById(user._id, "teams");
         if (!user) {
             throw new Error("用户不存在");
-        } 
+        }
         ctx.body = responseWrapper(user)
     }
 
     @request('post', '/api/user/resetPassword')
     @summary('通过邮箱重置密码')
     @tag
-    @body({email:{type:'string',required:true}})
+    @body({
+        email: {
+            type: 'string',
+            required: true
+        }
+    })
     static async resetPassword(ctx, next) {
         var body = ctx.request.body
 
-        var user = await User.findOne({ email:body.email },"-teams -password");
+        var user = await User.findOne({
+            email: body.email
+        }, "-teams -password");
         if (!user) {
             throw new Error("邮箱有误,没有该用户");
-        } 
+        }
 
-        var newPassword = Math.random().toString(36).substring(2, 5) + Math.random().toString(36).substring(2, 5);
+        var newPassword = Math
+            .random()
+            .toString(36)
+            .substring(2, 5) + Math
+            .random()
+            .toString(36)
+            .substring(2, 5);
         var hashPassword = await bcrypt.hash(newPassword, 10); // 10是 hash加密的级别, 默认是10，数字越大加密级别越高
-        await User.findByIdAndUpdate(user._id,{password:hashPassword})
-        Mail.send(['dzq1993@qq.com'],"爱发布密码重置邮件",`您的密码已重置${newPassword}`)
+        await User.findByIdAndUpdate(user._id, {password: hashPassword})
+        Mail.send(['dzq1993@qq.com'], "爱发布密码重置邮件", `您的密码已重置${newPassword}`)
         ctx.body = responseWrapper("密码已重置,并通过邮件发送到您的邮箱")
     }
 }

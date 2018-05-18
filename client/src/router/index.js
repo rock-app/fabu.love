aab004bb5741df8f1d0d7292b3a48caeb7a1a966
Darren-chenchen <1597887620@qq.com>
2018-05-18 11:48:43 +0800

    修改加载控件的方式

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import User from '../components/auth/auth.vue'
-import AppDetail from '../components/appDetail/appDetail.vue'
-import AppPreView from '../components/appDetail/appPreView.vue'
-import UserInfo from '../components/user/userInfo.vue'
-import Main from '../components/main/main.vue'
-import Apps from '../components/appList/appList.vue'
-import TeamMgr from '../components/team/teamMgr.vue'
+// import User from '../components/auth/auth.vue'
+// import AppDetail from '../components/appDetail/appDetail.vue'
+// import AppPreView from '../components/appDetail/appPreView.vue'
+// import UserInfo from '../components/user/userInfo.vue'
+// import Main from '../components/main/main.vue'
+// import Apps from '../components/appList/appList.vue'
+// import TeamMgr from '../components/team/teamMgr.vue'
 
 Vue.use(Router)
 
@@ -17,38 +17,38 @@ export default new Router({
     {
       path: '/',
       redirect: '/apps',
-      component: Main,
+      component: resolve => require(['@/components/main/main.vue'], resolve),
       children: [
         {
           path: '/apps',
-          component: Apps
+          component: resolve => require(['@/components/appList/appList.vue'], resolve)
         },
         {
           path: '/app/:appId',
           name: 'AppDetail',
-          component: AppDetail
+          component: resolve => require(['@/components/appDetail/appDetail.vue'], resolve)
         },
         {
           path: '/members',
           name: 'TeamMgr',
-          component: TeamMgr
+          component: resolve => require(['@/components/team/teamMgr.vue'], resolve)
         }
       ]
     },
     {
       path: '/login',
       name: 'Login',
-      component: User
+      component: resolve => require(['@/components/auth/auth.vue'], resolve)
     },
     {
       path: '/user',
       name: 'UserInfo',
-      component: UserInfo
+      component: resolve => require(['@/components/user/userInfo.vue'], resolve)
     },
     {
       path: '/:id',
       name: 'AppPreView',
-      component: AppPreView
+      component: resolve => require(['@/components/appDetail/appPreView.vue'], resolve)
     }
   ]
 })

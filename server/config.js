19f23deef7f4c70adae42dbac86f2f46595f43f8
wenshengling <wenshengling@hd123.com>
2019-04-26 13:59:56 +0800

    解析apkfix

--- server/config.js
+++ server/config.js
@@ -11,7 +11,7 @@ const common = {
     dbUser: process.env.FABU_DBUSER || undefined, //数据库用户 (没有开启mongodb用户认证的可以不填写)
     dbPass: process.env.FABU_DBPWD || undefined, //数据库密码 (没有开启mongodb用户认证的可以不填写)
     dbName: process.env.FABU_DB_NAME || "fabulove", //数据库名称
-    dbHost: process.env.FABU_DB_HOST || "localhost", //数据库地址
+    dbHost: process.env.FABU_DB_HOST || "0.0.0.0", //数据库地址
     dbPort: process.env.FABU_DB_PORT || "27017", //数据库端口
 
     emailService: process.env.FABU_EMAIL_SERVICE || "qq", //邮件相关配置 用于找回密码和邀请团队成员发送邮件

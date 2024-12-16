6a0bc7bf150c11150e5b718d297c42707337fe9c
answer-huang <answer-huang@users.noreply.github.com>
2024-12-17 00:49:31 +0800

    feat: update mongo version

--- server/helper/db.js
+++ server/helper/db.js
@@ -3,24 +3,22 @@ const Fawn = require("fawn");
 import config from '../config';
 
 let dbUrl = `mongodb://${ config.dbHost }:${ config.dbPort }/${ config.dbName }`;
-if (config.dbUser) {
-    dbUrl = `mongodb://${config.dbUser}:${config.dbPass}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+if ( config.dbUser ) {
+  dbUrl = `mongodb://${ config.dbUser }:${ config.dbPass }@${ config.dbHost }:${ config.dbPort }/${ config.dbName }`;
 }
 
 console.log(dbUrl)
-mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
-    if (err) {
-        console.log('Mongoose connection error: ' + err.message)
-    } else {
-        console.log('数据库连接成功')
-    }
+mongoose.connect(dbUrl).then((result) => {
+  console.log('数据库连接成功')
+}).catch((e) => {
+  console.log('Mongoose connection error: ' + e)
 })
 
 mongoose
-    .connection
-    .on('disconnected', function () {
-        console.log('Mongoose connection disconnected')
-    })
+  .connection
+  .on('disconnected', function () {
+    console.log('Mongoose connection disconnected')
+  })
 Fawn.init(dbUrl);
 
 module.exports = mongoose
--- server/model/message.js
+++ server/model/message.js
@@ -1,31 +1,31 @@
 const mongoose = require('../helper/db')
 const Schema = mongoose.Schema
 
-var messageSchema = new Schema({
+const messageSchema = new Schema({
     category: {
-        type:String,
-        required:true //消息类型  //INVITE 邀请
+        type: String,
+        required: true //消息类型  //INVITE 邀请
     },
-    content:{
-        type:String,
-        required:true  //消息内容
+    content: {
+        type: String,
+        required: true  //消息内容
     },
-    sender:String, //发送者的id
-    receiver:{ 
-        type:String,  //接受者的id 必填
-        required:true
+    sender: String, //发送者的id
+    receiver: {
+        type: String,  //接受者的id 必填
+        required: true
     },
-    sendAt:{
-        type:Date,
-        default:Date.now()
+    sendAt: {
+        type: Date,
+        default: Date.now()
     },
-    status:{
-        type:String,
-        default:'unread',
-        enum:['unread','hasread']
+    status: {
+        type: String,
+        default: 'unread',
+        enum: [ 'unread', 'hasread' ]
     },
-    data:String     //消息中带有的data字段
+    data: String     //消息中带有的data字段
 
-})
+});
 
 module.exports = mongoose.model('Message', messageSchema);
